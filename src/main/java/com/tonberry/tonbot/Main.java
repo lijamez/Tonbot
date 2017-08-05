bf3c6501b91db7e4e45996e00089e8d0228346bd
James Li <lijamez@users.noreply.github.com>
2017-08-05 14:29:49 -0700

    Add support for supplying prefix as command line option

--- src/main/java/com/tonberry/tonbot/Main.java
+++ src/main/java/com/tonberry/tonbot/Main.java
@@ -6,12 +6,17 @@ import com.google.inject.Injector;
 import com.tonberry.tonbot.modules.coinflip.CoinFlipModule;
 import com.tonberry.tonbot.modules.diagnostics.DiscordDiagnosticsModule;
 import com.tonberry.tonbot.modules.tmdb.TMDbModule;
+import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
 
 public class Main {
 
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    private static final String DEFAULT_PREFIX = "t!";
 
     public static void main(String[] args) {
         String discordBotToken = System.getProperty("discordBotToken");
@@ -20,8 +25,27 @@ public class Main {
         String tmdbApiKey = System.getProperty("tmdbApiKey");
         Preconditions.checkNotNull(tmdbApiKey, "tmdbApiKey system property must be set.");
 
+        CommandLineParser parser = new DefaultParser();
+
+        // Read CLI options
+        Options cliOptions = new Options();
+        cliOptions.addOption("p","prefix", true, "specify the prefix which the bot will respond to");
+
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(cliOptions, args);
+        } catch (ParseException e) {
+            LOG.error("Unable to parse command line arguments.", e);
+            System.exit(1);
+        }
+
+        String prefix = cmd.getOptionValue("prefix");
+        if (prefix == null) {
+            prefix = DEFAULT_PREFIX;
+        }
+
         Injector injector = Guice.createInjector(
-                new TonbotModule(discordBotToken, "t!"),
+                new TonbotModule(discordBotToken, prefix),
                 new DiscordDiagnosticsModule(),
                 new CoinFlipModule(),
                 new TMDbModule(tmdbApiKey));
