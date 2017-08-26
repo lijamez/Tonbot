3bc9feba7bdbb4f9c3bf83d1eaac309422cfeed8
James Li <lijamez@users.noreply.github.com>
2017-08-26 14:24:01 -0700

    Implemented sendMessageSync for BotUtils

--- src/main/java/net/tonbot/core/BotUtilsImpl.java
+++ src/main/java/net/tonbot/core/BotUtilsImpl.java
@@ -1,12 +1,18 @@
 package net.tonbot.core;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 import com.google.common.base.Preconditions;
 import com.google.inject.Inject;
 
 import net.tonbot.common.BotUtils;
+import net.tonbot.common.TonbotTechnicalFault;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.internal.json.objects.EmbedObject;
 import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.util.RequestBuffer;
 import sx.blah.discord.util.RequestBuilder;
 
 class BotUtilsImpl implements BotUtils {
@@ -31,6 +37,22 @@ class BotUtilsImpl implements BotUtils {
 					return true;
 				})
 				.execute();
+	}
+
+	@Override
+	public IMessage sendMessageSync(IChannel channel, String message) {
+		Preconditions.checkNotNull(channel, "channel must be non-null.");
+		Preconditions.checkNotNull(message, "message must be non-null.");
+
+		Future<IMessage> sentMessage = RequestBuffer.request(() -> {
+			return channel.sendMessage(message);
+		});
+
+		try {
+			return sentMessage.get();
+		} catch (InterruptedException | ExecutionException e) {
+			throw new TonbotTechnicalFault("Unable to send message.", e);
+		}
 	}
 
 	@Override
